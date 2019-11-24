2fa78533ce200a0d1ae5641a7ce84eeedca1dbfa
nghichcode <nghichcode@gmail.com>
2019-11-24 20:48:01 +0700

    NextStage=1.1

--- bai5/bai5.py
+++ bai5/bai5.py
@@ -1,14 +1,17 @@
 import numpy as np
 from matplotlib.pyplot import imread, imsave, imshow, show
 
-c = 3
 n = 8
+c = 6 # c*8 pixel
 
-chess = np.zeros( (n,n), dtype='int8' )
+chess = np.zeros( (n,n), dtype='uint8' )
 chess[1::2,::2] = 1
 chess[::2,1::2] = 1
 
 chrp = np.repeat(chess, c, axis=0)
 chrp = np.repeat(chrp, c, axis=1)
 
-imshow(chrp)
+chess_bw = np.zeros((chrp.shape[0],chrp.shape[0],3), dtype='uint8')
+chess_bw[...,0]=chess_bw[...,1]=chess_bw[...,2]=np.copy(chrp)*254
+
+imsave('chess_blackwhite.png',chess_bw)
